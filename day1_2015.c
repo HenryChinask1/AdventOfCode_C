82d8684ecd3c7dafb5405b11ddfa7713b3ea4c3b
HenryChinask1 <jeffbaier72@yahoo.com>
2026-01-02 15:03:34 -0500

    Small refactor to only open the file once.

--- day1_2015.c
+++ day1_2015.c
@@ -4,11 +4,12 @@
 // TODO: Figure out how to separate the projects and puzzle days.
 int main(void)
 {
+    // Part One.
+
     FILE *fp;
     fp = fopen("Advent of Code Inputs/2015day1.txt", "r");
     int c;
     int floor = 0;
-    int step = 0;
 
     while((c = fgetc(fp)) != EOF) {
         if ((char)c == '(') {
@@ -18,17 +19,18 @@ int main(void)
         }
     }
     printf("2015 Part One: %d\n", floor);
-    fclose(fp);
-
-
-    fp = fopen("Advent of Code Inputs/2015day1.txt", "r");
+    
+    // Part 2.
+    
+    c = fseek(fp, 0, SEEK_SET);
     floor = 0;
-    int d;
-    while ((d = fgetc(fp)) != EOF) {
+    int step = 0;
+
+    while ((c = fgetc(fp)) != EOF) {
         if (floor == -1) {
             printf("2015 Part Two: %d\n", step);
             break;
-        } else if ((char)d == '(') {
+        } else if ((char)c == '(') {
             floor += 1;
             step += 1;
         } else {
