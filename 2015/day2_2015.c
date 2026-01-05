9d77760405a1c4ca2154da8ff31908e07de10e53
HenryChinask1 <jeffbaier72@yahoo.com>
2026-01-05 15:37:42 -0500

    part one complete

--- 2015/day2_2015.c
+++ 2015/day2_2015.c
@@ -6,9 +6,9 @@ int main(void)
     int l, w, h = 0;
     int total = 0;
     FILE *fp = fopen("../Advent of Code Inputs/2015day2.txt", "r");
-    char lines[20];
+    char lines[10];
 
-    while (fgets(lines, 20, fp) != '\0') {
+    while (fgets(lines, 10, fp) != '\0') {
         sscanf(lines, "%dx%dx%d", &l, &w, &h);
         total += ((l * w * 2) + (w * h * 2) + (l * h * 2));
         int min_val = (l * w);
